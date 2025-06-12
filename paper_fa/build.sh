ab670bf39134c9954bd805afec7172f32a0aa75e
Kasra Fouladi <k4sr405@gmail.com>
2025-06-12 22:35:33 +0330

    Delete paper_fa directory

--- paper_fa/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-xelatex paper_of_the_project.tex
-bibtex paper_of_the_project
-xelatex paper_of_the_project.tex
-xelatex paper_of_the_project.tex
-rm -f *.aux *.bbl *.blg *.log *.out *.toc
